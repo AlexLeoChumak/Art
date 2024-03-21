10d5c0542ea587d7d9417ff2310c9c7bed12358d
AlexLeoChumak <chumak1106@gmail.com>
2024-03-21 14:17:05 +0300

    edit CategoryNavbarComponent

--- src/app/layouts/category-navbar/category-navbar.component.ts
+++ src/app/layouts/category-navbar/category-navbar.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Category } from 'src/app/models/category';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-category-navbar',
   templateUrl: './category-navbar.component.html',
-  styleUrls: ['./category-navbar.component.scss']
+  styleUrls: ['./category-navbar.component.scss'],
 })
-export class CategoryNavbarComponent {
+export class CategoryNavbarComponent implements OnInit {
+  categoryArray!: Category[];
 
+  constructor(
+    private categoriesService: CategoriesService,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.categoriesService.loadData().subscribe({
+      next: (data) => {
+        this.categoryArray = data;
+      },
+      error: (err) => {
+        this.toastr.error(err);
+      },
+    });
+  }
 }
