2e34c8315ae1203429801e7d506c976135a83a01
AlexLeoChumak <chumak1106@gmail.com>
2024-03-21 14:58:44 +0300

    edit CategoryNavbarComponent

--- src/app/layouts/category-navbar/category-navbar.component.ts
+++ src/app/layouts/category-navbar/category-navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { Subscription, catchError, throwError } from 'rxjs';
 import { Category } from 'src/app/models/category';
 import { CategoriesService } from 'src/app/services/categories.service';
 
@@ -8,8 +9,9 @@ import { CategoriesService } from 'src/app/services/categories.service';
   templateUrl: './category-navbar.component.html',
   styleUrls: ['./category-navbar.component.scss'],
 })
-export class CategoryNavbarComponent implements OnInit {
+export class CategoryNavbarComponent implements OnInit, OnDestroy {
   categoryArray!: Category[];
+  loadDataSub!: Subscription;
 
   constructor(
     private categoriesService: CategoriesService,
@@ -17,13 +19,26 @@ export class CategoryNavbarComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.categoriesService.loadData().subscribe({
-      next: (data) => {
-        this.categoryArray = data;
-      },
-      error: (err) => {
-        this.toastr.error(err);
-      },
-    });
+    this.loadDataSub = this.categoriesService
+      .loadData()
+      .pipe(
+        catchError((err) => {
+          return throwError(() => err);
+        })
+      )
+      .subscribe({
+        next: (data) => {
+          this.categoryArray = data;
+        },
+        error: (err) => {
+          this.toastr.error(err);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.loadDataSub) {
+      this.loadDataSub.unsubscribe();
+    }
   }
 }
