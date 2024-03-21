21c19b15caad2a3d68dc364453fe7742a1290aae
AlexLeoChumak <chumak1106@gmail.com>
2024-03-21 14:57:50 +0300

    create interface Post

---
+++ src/app/models/post.ts
@@ -0,0 +1,15 @@
+import { Category } from './category';
+
+export interface Post {
+  title: string;
+  permalink: string;
+  excerpt: string;
+  category: Category;
+  postImgUrl: string;
+  content: string;
+  isFeatured: boolean;
+  views: number;
+  status: string;
+  createdAt: Date;
+  id: string;
+}
