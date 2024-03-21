764ebcfb89d38e1fa6806df3b71800c5b5e0900a
AlexLeoChumak <chumak1106@gmail.com>
2024-03-21 14:59:03 +0300

    edit HomeComponent

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, catchError, throwError } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  loadPostsSub!: Subscription;
 
+  constructor(
+    private postsService: PostsService,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadPostsSub = this.postsService
+      .loadPosts()
+      .pipe(
+        catchError((err) => {
+          return throwError(() => err);
+        })
+      )
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+        },
+        error: (err) => {
+          this.toastr.error(err);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.loadPostsSub) {
+      this.loadPostsSub.unsubscribe();
+    }
+  }
 }
