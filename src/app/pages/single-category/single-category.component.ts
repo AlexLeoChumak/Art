1f6bd889be8d9ee6bbb482f4c7ac957d4576dcc9
AlexLeoChumak <chumak1106@gmail.com>
2024-03-24 12:02:41 +0300

    add call route.params and .postsService.loadCategoryPosts, add method ngOnDestroy

--- src/app/pages/single-category/single-category.component.ts
+++ src/app/pages/single-category/single-category.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import {
+  Subject,
+  catchError,
+  switchMap,
+  takeUntil,
+  tap,
+  throwError,
+} from 'rxjs';
+import { Post } from 'src/app/models/post';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-single-category',
   templateUrl: './single-category.component.html',
-  styleUrls: ['./single-category.component.scss']
+  styleUrls: ['./single-category.component.scss'],
 })
-export class SingleCategoryComponent {
+export class SingleCategoryComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject<void>();
+  categoryPosts!: Post[];
 
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .pipe(
+        switchMap((data) => {
+          return this.postsService.loadCategoryPosts(data['id']).pipe(
+            tap((val) => console.log('val', val)),
+            catchError((err) => throwError(() => err))
+          );
+        }),
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe({
+        next: (data) => {
+          this.categoryPosts = data;
+        },
+        error: (err) => this.toastr.error(err),
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
