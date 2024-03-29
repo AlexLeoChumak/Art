f0ecd144eefd702e3c34a8360d7a74cbdde6d76f
AlexLeoChumak <chumak1106@gmail.com>
2024-03-29 20:40:03 +0300

    add Logout

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription, catchError, throwError } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  getStorageSub!: Subscription;
+  logoutSub!: Subscription;
 
+  constructor(
+    private authService: AuthService,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  logout() {
+    this.logoutSub = this.authService
+      .logout()
+      .pipe(
+        catchError((err) => {
+          return throwError(() => err);
+        })
+      )
+      .subscribe({
+        next: () => {
+          this.toastr.success('Logout successful');
+          this.router.navigate(['/login']);
+        },
+        error: (err) => {
+          this.toastr.error(err);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.getStorageSub ? this.getStorageSub.unsubscribe() : null;
+  }
 }
