8e0182cb3a5b33af0fc4c30b388cd00435fdd746
AlexLeoChumak <chumak1106@gmail.com>
2024-04-02 19:21:18 +0300

    clear unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, catchError, throwError } from 'rxjs';
-import { AuthService } from './services/auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
